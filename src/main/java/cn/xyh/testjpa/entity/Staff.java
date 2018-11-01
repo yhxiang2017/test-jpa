b34d3f7ce7cee9d975537c95057eecfe3c09cd9a
xiangyh0503 <1319275241@qq.com>
2018-11-01 20:11:18 +0800

    1111

--- src/main/java/cn/xyh/testjpa/entity/Staff.java
+++ src/main/java/cn/xyh/testjpa/entity/Staff.java
@@ -3,6 +3,7 @@ package cn.xyh.testjpa.entity;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
 public class Staff {
@@ -13,7 +14,37 @@ public class Staff {
 
     private String staffName;
 
+    private String address;
+
+    private Date inTime;
+
+    private Date endTime;
+
     private int companyId;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getInTime() {
+        return inTime;
+    }
+
+    public void setInTime(Date inTime) {
+        this.inTime = inTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
 
     public int getStaffId() {
         return staffId;
